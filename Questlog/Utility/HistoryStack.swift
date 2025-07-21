b299128301566e51e4db2d9b8b46534d8725eadf
Your Name <you@email.com>
2025-07-21 14:42:06 -0500

    Created a file to contain the class structure for a history stack to make navigation much easier

---
+++ Questlog/Utility/HistoryStack.swift
@@ -0,0 +1,8 @@
+//
+//  HistoryStack.swift
+//  Questlog
+//
+//  Created by Kaitlyn Martin on 7/21/25.
+//
+
+import Foundation
---
+++ Questlog/Views/SettingsView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsView.swift
+//  Questlog
+//
+//  Created by Kaitlyn Martin on 7/19/25.
+//
+
+import Foundation
